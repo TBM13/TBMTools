446c0fba21e6a938b0baa4e69be2220cf2f803a8
Mateo Trueba <mateo.trueba@gmail.com>
2019-10-21 13:45:04 -0300

    buildLOS: Ability to set recovery variant to TWRP and build it

--- buildLOS.sh
+++ buildLOS.sh
@@ -39,5 +39,11 @@ printInfo "Lunching device..."
 lunch lineage_$device-userdebug || showError
 printf '\033[0;32m\nFinished lunch successfully\n\n\033[0m'
 
-printInfo "Starting $param build..."
+if [ "$mkaArgument" == "twrp" ]; then
+	export RECOVERY_VARIANT=twrp
+	mkaArgument=recoveryimage
+fi
+
+printInfo "Starting $mkaArgument build..."
+
 mka $mkaArgument | tee build.log
