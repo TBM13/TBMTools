4d30a13028d185a41c88b9aab75c8b194393af0b
TBM <mateo.trueba@gmail.com>
2021-04-11 13:57:40 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,14 +6,20 @@ showError()
 	exit 1
 }
 
-export CROSS_COMPILE=~/Escritorio/toolchains/arm-eabi-4.8/bin/arm-eabi-
+#arm:
+arm_48=~/Desktop/Common/arm-eabi-4.8/bin/arm-eabi-
+#arm64:
+aarch64_linaro_650=~/Desktop/Common/gcc-linaro-6.5.0-2018.12-i686_aarch64-linux-gnu/bin/aarch64-linux-gnu-
+
+export CROSS_COMPILE=$arm_48
 export ARCH=arm
+#export ANDROID_MAJOR_VERSION=q
 
 make lineage_fame_defconfig || showError
 printf '\033[0;32m\nDefconfig built successfully\n\n\033[0m'
 
 make ARCH=$ARCH -j4 || showError
-printf '\033[0;32m\nzImage built successfully\n\033[0m'
+printf '\033[0;32m\nKernel Image built successfully\n\033[0m'
 
 if [ -f "./customRepack.sh" ]; then
 	./customRepack.sh $ARCH
